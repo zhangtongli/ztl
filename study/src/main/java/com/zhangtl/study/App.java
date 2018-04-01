7e106e3a66fdf5c717abacaa1cfcd51cc6bb89ed
ure52 <ure52@windows10.microdone.cn>
2018-04-01 09:53:11 +0800

    h

---
+++ study/src/main/java/com/zhangtl/study/App.java
@@ -0,0 +1,13 @@
+package com.zhangtl.study;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
